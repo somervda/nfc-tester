5dbac4c91fb6c1c07ec9ce1633198fe4fe6aaa5f
David <39265971+somervda@users.noreply.github.com>
2022-04-03 16:33:47 -0400

    Added network and http logic to trigger alexa routine when any nfc scanned

--- main.py
+++ main.py
@@ -7,7 +7,26 @@ import ssd1306
 import pn532_i2c
 import time
 from micropython import const
+import network
+import gc
+# MicroWebCli is a micro HTTP Web client for MicroPython see https://github.com/jczic/MicroWebCli
+from microWebCli import MicroWebCli
 
+ALEXA_LAMP_URL = "http://192.168.1.115:1880/alexa"
+# ALEXA_LAMP_URL = "http://192.168.1.115:1880"
+
+# Setup network connection
+sta_if = network.WLAN(network.STA_IF)
+if not sta_if.isconnected():
+    print('connecting to guest network...')
+    sta_if.active(True)
+    sta_if.connect('guest24', 'backyard')
+    while not sta_if.isconnected():
+        time.sleep_ms(500)
+        print("*", end="")
+        pass
+print('network config:', sta_if.ifconfig())
+gc.collect()
 
 # Set up I2C buss and scan for devices
 i2c = machine.I2C(1, scl=machine.Pin(15), sda=machine.Pin(4))
@@ -39,6 +58,7 @@ pn532.SAM_configuration()
 
 # Start listening for a card
 print("Waiting for RFID/NFC card...")
+gc.collect()
 last_uid = None
 while True:
     # Check if a card is available to read
@@ -53,6 +73,7 @@ while True:
     if last_uid == uid:
         pass
     else:
+        gc.collect()
         # Process new RFID card
         oled.fill(0)
         # Format short string showing UID
@@ -69,24 +90,31 @@ while True:
         # For NCF cards pull data to check for NDEF record
         # print("len(uid):", len(uid))
         if len(uid) == 7:
-            # Get first 176 bytes of data on the card
-            cardData = pn532.mifare_classic_read_block(0)
-            for blockNumber in range(3, 43, 4):
-                cardData += pn532.mifare_classic_read_block(blockNumber)
-            # Hex dump
-            # Parphrased from https://www.geoffreybrown.com/blog/a-hexdump-program-in-python/
-            print()
-            print("Hex Dump for ", [hex(i) for i in uid])
-            n = 0
-            for hd in range(11):
-                b = cardData[(hd*16):((hd*16)+16)]
-                s1 = " ".join([f"{i:02x}" for i in b])  # hex string
-                # insert extra space between groups of 8 hex values
-                s1 = s1[0:23] + " " + s1[23:]
-                # ascii string; chained comparison
-                s2 = "".join([chr(i) if 32 <= i <= 127 else "." for i in b])
-                print(f"{n * 16:08x}  {s1:<48}  |{s2}|")
-                n += 1
+            # # Get first 176 bytes of data on the card
+            # cardData = pn532.mifare_classic_read_block(0)
+            # for blockNumber in range(3, 43, 4):
+            #     cardData += pn532.mifare_classic_read_block(blockNumber)
+            # # Hex dump
+            # # Parphrased from https://www.geoffreybrown.com/blog/a-hexdump-program-in-python/
+            # print()
+            # print("Hex Dump for ", [hex(i) for i in uid])
+            # n = 0
+            # for hd in range(11):
+            #     b = cardData[(hd*16):((hd*16)+16)]
+            #     s1 = " ".join([f"{i:02x}" for i in b])  # hex string
+            #     # insert extra space between groups of 8 hex values
+            #     s1 = s1[0:23] + " " + s1[23:]
+            #     # ascii string; chained comparison
+            #     s2 = "".join([chr(i) if 32 <= i <= 127 else "." for i in b])
+            #     print(f"{n * 16:08x}  {s1:<48}  |{s2}|")
+            #     n += 1
+            print("Found card with UID:", [hex(i) for i in uid])
+            try:
+                # Send request to elexa no mater what the card ID is
+                resp1 = MicroWebCli.GETRequest(
+                    ALEXA_LAMP_URL, connTimeoutSec=3)
+            except:
+                print("http get failed")
         else:
             print()
             print("Found card with UID:", [hex(i) for i in uid])
