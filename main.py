78dedc78cdb92f2d1ddf9800a14d4609c792ea4b
David <39265971+somervda@users.noreply.github.com>
2022-04-11 13:52:33 -0400

    Little clean up code so it recovers from half reads, sun spots etc

--- main.py
+++ main.py
@@ -72,80 +72,93 @@ time.sleep_ms(1000)
 print("Waiting for RFID/NFC card...")
 gc.collect()
 last_uid = None
+bToggle = True
 while True:
-    # Check if a card is available to read
-    uid = pn532.read_passive_target(timeout=0.5)
-    print(".", end="")
-    # Try again if no card is available
-    if uid is None:
-        last_uid = None
-        oled.fill(0)
-        oled.show()
-        continue
-    if last_uid == uid:
-        pass
+    time.sleep_ms(300)
+    oled.fill(0)
+    if bToggle:
+        oled.text("* Wait for NFC *", 0, 10)
+        bToggle = False
     else:
-        gc.collect()
-        # Process new RFID card
-        oled.fill(0)
-        # Format short string showing UID
-        suid = ""
-        for i in uid:
-            if len(suid) > 0:
-                suid += ":"
-            s = hex(i)[2:4]
-            suid += s
-        # Display result on OED
-        oled.text('UID Found', 0, 0)
-        oled.text(suid, 0, 10)
-        oled.show()
-        # For NCF cards pull data to check for NDEF record
-        # print("len(uid):", len(uid))
-        if len(uid) == 7:
-            # # Get first 176 bytes of data on the card
-            try:
-                cardData = pn532.mifare_classic_read_block(0)
-                for blockNumber in range(3, 43, 4):
-                    cardData += pn532.mifare_classic_read_block(blockNumber)
-            except:
-                print("Failed block read")
-                pass
-            print("Found card with UID:", [hex(i) for i in uid], uid)
-            #  If card has a NDEF URL data
-            #  First check card has a TLV block
-            if (cardData[19] == TLV_START and cardData[20] == TLV_TYPE_NDEF):
-                # Check TLV block has a NDEF record
-                if (cardData[22] == NDEF_HEADER_WELL_KNOWN_TYPE and cardData[23] == 0x01):
-                    # Check if data is URI
-                    if (cardData[25] == NDEF_PAYLOAD_TYPE_URI):
-                        # Get URI length
-                        uriLength = cardData[24]
-                        # Add the URL prefix
-                        if cardData[26] == NDEF_URIPREFIX_HTTP_WWWDOT:
-                            sURL = "http://www."
-                        if cardData[26] == NDEF_URIPREFIX_HTTPS_WWWDOT:
-                            sURL = "https://www."
-                        if cardData[26] == NDEF_URIPREFIX_HTTP:
-                            sURL = "http://"
-                        if cardData[26] == NDEF_URIPREFIX_HTTPS:
-                            sURL = "https://"
-                        sURL += (cardData[27: uriLength -
-                                          1 + 27]).decode('utf-8')
-                        oled.text(sURL, 0, 20)
-                        oled.show()
-                        #  Call the URL
-                        try:
-                            print("Calling: ", sURL)
-                            resp1 = MicroWebCli.GETRequest(
-                                sURL, connTimeoutSec=3)
-                            oled.text("Success!", 0, 30)
-                            oled.show()
-                        except:
-                            print("http get failed")
-                            oled.text("URL Failed!", 0, 30)
-                            oled.show()
+        bToggle = True
+    oled.show()
+    try:
+        # Check if a card is available to read
+        uid = pn532.read_passive_target(timeout=0.2)
+        print(".", end="")
+        # Try again if no card is available
+        if uid is None:
+            last_uid = None
+            oled.fill(0)
+            oled.show()
+            continue
+        if last_uid == uid:
+            pass
         else:
-            print()
-            print("Found card with UID:", [hex(i) for i in uid])
-
-    last_uid = uid
+            gc.collect()
+            # Process new RFID card
+            oled.fill(0)
+            # Format short string showing UID
+            suid = ""
+            for i in uid:
+                if len(suid) > 0:
+                    suid += ":"
+                s = hex(i)[2:4]
+                suid += s
+            # Display result on OED
+            oled.text('UID Found', 0, 0)
+            oled.text(suid, 0, 10)
+            oled.show()
+            # For NCF cards pull data to check for NDEF record
+            # print("len(uid):", len(uid))
+            if len(uid) == 7:
+                # # Get first 176 bytes of data on the card
+                try:
+                    cardData = pn532.mifare_classic_read_block(0)
+                    for blockNumber in range(3, 43, 4):
+                        cardData += pn532.mifare_classic_read_block(
+                            blockNumber)
+                except:
+                    print("Failed block read")
+                    pass
+                print("Found card with UID:", [hex(i) for i in uid], uid)
+                #  If card has a NDEF URL data
+                #  First check card has a TLV block
+                if (cardData[19] == TLV_START and cardData[20] == TLV_TYPE_NDEF):
+                    # Check TLV block has a NDEF record
+                    if (cardData[22] == NDEF_HEADER_WELL_KNOWN_TYPE and cardData[23] == 0x01):
+                        # Check if data is URI
+                        if (cardData[25] == NDEF_PAYLOAD_TYPE_URI):
+                            # Get URI length
+                            uriLength = cardData[24]
+                            # Add the URL prefix
+                            if cardData[26] == NDEF_URIPREFIX_HTTP_WWWDOT:
+                                sURL = "http://www."
+                            if cardData[26] == NDEF_URIPREFIX_HTTPS_WWWDOT:
+                                sURL = "https://www."
+                            if cardData[26] == NDEF_URIPREFIX_HTTP:
+                                sURL = "http://"
+                            if cardData[26] == NDEF_URIPREFIX_HTTPS:
+                                sURL = "https://"
+                            sURL += (cardData[27: uriLength -
+                                              1 + 27]).decode('utf-8')
+                            oled.text(sURL, 0, 20)
+                            oled.show()
+                            #  Call the URL
+                            try:
+                                print("Calling: ", sURL)
+                                resp1 = MicroWebCli.GETRequest(
+                                    sURL, connTimeoutSec=3)
+                                # oled.text("Success!", 0, 30)
+                                # oled.show()
+                            except:
+                                print("http get failed")
+                                # oled.text("URL Failed!", 0, 30)
+                                # oled.show()
+            else:
+                print()
+                print("Found card with UID:", [hex(i) for i in uid])
+        last_uid = uid
+    except:
+        print("Something failed")
+        pass
